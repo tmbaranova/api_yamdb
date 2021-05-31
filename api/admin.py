df62d9962162f5bfa07e86e5ed4d18d161a40ca9
Viktoria <viktrols@yandex.ru>
2021-05-31 21:02:50 +0300

    Add models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Title
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    empty_value_display = '-empty-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    empty_value_display = '-empty-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'year',)
+    empty_value_display = '-empty-'
--- api/models.py
+++ api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField('Genre')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL,)
+
+    class Meta:
+        ordering = ['-id',]
+
+    def __str__(self):
+        return self.name
